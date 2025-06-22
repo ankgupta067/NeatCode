bbd71e4bbc47f2aabd95c3f14bfdb63c440ba569
ankushgupta <ankushgupta@salesforce.com>
2025-06-22 15:34:46 +0530

    update readme

--- NeatCode/src/main/java/org/example/workhours.java
+++ NeatCode/src/main/java/org/example/workhours.java
@@ -1,4 +1,62 @@
 package org.example;
 
-public class workhours {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.toList;
+
+
+class      Result {
+//08???40 -> 12
+
+    //-workhors - 12 (0,8) -> 0_ _1 _
+// 3 _ _ _ 0 4 8 0 5 7 -> 1 4 7
+//0 5 7
+    /*
+     * Complete the 'findSchedules' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER workHours
+     *  2. INTEGER dayHours
+     *  3. STRING pattern
+     */
+//sum , indx
+// 4 -> 8
+    public static List<String> findSchedules(int workHours, int dayHours, String pattern) {
+
+    }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int workHours = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int dayHours = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String pattern = bufferedReader.readLine();
+
+        List<String> result = Result.findSchedules(workHours, dayHours, pattern);
+
+        bufferedWriter.write(
+                result.stream()
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NeatCode
+
+noition ->  https://www.notion.so/Coding-Question-Log-1f5f9927abb880bb8494d6dc92675797
+
+Neatcode -> https://neetcode.io/practice?tab=neetcode150
